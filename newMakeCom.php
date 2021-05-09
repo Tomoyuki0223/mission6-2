aab4481c0da6aedca735683fe36d6cfa53f980c6
Tomo <76682326+Tomoyuki0223@users.noreply.github.com>
2021-05-09 15:51:15 +0900

    Create newMakeCom.php

---
+++ newMakeCom.php
@@ -0,0 +1,21 @@
+<?php 
+    include($_SERVER['DOCUMENT_ROOT']."/left.php");
+    require 'db.php';
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>新規登録 | TECH-BASE掲示板</title>
+    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+  </head>
+  <body>
+  <div class="right">
+    <div class="user">
+      <p>登録完了しました</p>
+      <a href="home.php" class="optional">ホームに戻る</a><br>
+    </div>
+  </div>
+  </body>
+</html>
