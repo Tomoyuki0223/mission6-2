b8ac42841131b6977db137e73901ab85bd2048d9
Tomo <76682326+Tomoyuki0223@users.noreply.github.com>
2021-07-01 10:36:47 +0900

    Update seCom.php

--- seCom.php
+++ seCom.php
@@ -1,21 +1,26 @@
 <?php 
-    include($_SERVER['DOCUMENT_ROOT']."/left.php");
-    require 'db.php';
+// データベースに接続
+$dsn = 'データベース名';
+$user = 'ユーザー名';
+$password = 'パスワ－ド';
+$pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+
+include($_SERVER['DOCUMENT_ROOT']."/mission6_2/left.php");
 ?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>ログイン | TECH-BASE掲示板</title>
-    <link rel="stylesheet" href="stylesheet.css">
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-  </head>
-  <body>
-  <div class="right">
-    <div class="user">
-      <p>すでにログイン中です</p>
-      <a href="seOut.php" class="optional">ログアウト</a><br>
-    </div>
+<head>
+<meta charset="utf-8">
+<title>ログイン | TECH-BASE掲示板</title>
+<link rel="stylesheet" href="stylesheet.css">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+</head>
+<body>
+<div class="right">
+  <div class="user">
+    <p>すでにログイン中です</p>
+    <a href="seOut.php" class="optional">ログアウト</a><br>
   </div>
-  </body>
+</div>
+</body>
 </html>
