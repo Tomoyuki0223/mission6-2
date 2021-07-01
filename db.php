c0800862ca801ae7e8534f499f0517119173245f
Tomo <76682326+Tomoyuki0223@users.noreply.github.com>
2021-07-01 10:25:00 +0900

    Delete db.php

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php
-    /*DB接続開始*/
-    $dsn = 'mysql:dbname=tb221121db;host=localhost';
-    $user = 'ユーザー名';
-    $password = 'パスワード';
-    $pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
-    /*DB接続完了*/
-?>
